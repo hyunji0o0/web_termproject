419616878fd1dbb1d11ef32aeb0fbd50136e3c0c
김현지 <hyunji051127@gmail.com>
2025-11-16 19:12:13 +0900

    타이틀+버튼

--- front/script.js
+++ front/script.js
@@ -1,9 +1,7 @@
 const whiteSection = document.getElementById("white-section");
 
 window.addEventListener("scroll", () => {
-    const scrollY = window.scrollY;
-
-    if (scrollY > 50) {
+    if (window.scrollY > 50) {
         whiteSection.classList.add("active");
     } else {
         whiteSection.classList.remove("active");
