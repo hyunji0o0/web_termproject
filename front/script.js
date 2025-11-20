247b095d5ee809771b7fdc3c442e5b13b240bdad
김현지 <hyunji051127@gmail.com>
2025-11-20 19:18:48 +0900

    배경 gradient 추가

--- front/script.js
+++ front/script.js
@@ -56,38 +56,77 @@ function calculateBMI() {
     document.getElementById("bmi-status-text").textContent = status;
 }
 
+// hour dropdown 자동 생성
+window.addEventListener("DOMContentLoaded", () => {
+    const hourSelect = document.getElementById("activity-hour");
+    for (let i = 1; i <= 23; i++) {
+        const opt = document.createElement("option");
+        opt.value = i;
+        opt.textContent = `${i} 시간`;
+        hourSelect.appendChild(opt);
+    }
+});
+
 
 // 기초대사량/BMI 버튼 클릭 →  기초대사량/bmi 로 이동
 document.querySelector(".gradient-buttons button:nth-child(1)")
     .addEventListener("click", () => {
-        document.getElementById("white-section").scrollIntoView({
+        document.getElementById("bmi-section").scrollIntoView({
             behavior: "smooth"
         });
     });
 
-function calculateActivity() { 
-    const weight = parseFloat(document.getElementById("activity-select").value);
+// 식품영양성분 계산 버튼 클릭 → 영양성분 계산으로 이동
+document.querySelector(".gradient-buttons button:nth-child(2)")
+    .addEventListener("click", () => {
+        document.getElementById("food-section").scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+
+// 활동대사량 버튼 클릭 →  활동대사량 계산기로 이동
+document.querySelector(".gradient-buttons button:nth-child(3)")
+    .addEventListener("click", () => {
+        document.getElementById("activity-section").scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+
+function calculateActivity() {
+    const weight = parseFloat(document.getElementById("activity-weight").value);
     const met = parseFloat(document.getElementById("activity-select").value);
 
-    const hour = parseFloat(document.getElementById("activity-hour").value) || 0;
-    const minute = parseFloat(document.getElementById("activity-minute").value) || 0;
+    const hour = parseInt(document.getElementById("activity-hour").value);
+    const minute = parseInt(document.getElementById("activity-minute").value);
+
+    // 필수 입력 체크
+    if (!weight || weight <= 0) {
+        alert("체중을 올바르게 입력해주세요!");
+        return;
+    }
+
+    if (!met) {
+        alert("운동 종목을 선택해주세요!");
+        return;
+    }
+
+    // minute 입력 검증
+    if (isNaN(minute) || minute < 0 || minute > 59) {
+        alert("분(minute)은 0~59 사이로 입력해주세요!");
+        return;
+    }
 
     const totalMinutes = hour * 60 + minute;
 
-    if (!met || totalMinutes <= 0) {
-        alert("운동 종목과 운동 시간을 입력해주세요!");
+    if (totalMinutes <= 0) {
+        alert("운동 시간을 입력해주세요!");
         return;
     }
 
-    if (minute >= 60 || hour >= 24) {
-        alert("minute은 0~59 사이 / hour은 0~23 사이의 수를 입력해주세요!");
-        return;
-    }
-
-
-    // kcal = 0.0175 × MET × 체중(kg) × 시간(분)
+    // kcal 계산
     const kcal = (0.0175 * met * weight * totalMinutes).toFixed(2);
 
     document.getElementById("activity-value").innerText = kcal;
 }
+
 
