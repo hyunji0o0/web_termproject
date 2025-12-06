729e2dfeb12cf187ac0d08feda08fef57c7a3eee
unknown <thsgustn4588@pusan.ac.kr>
2025-12-07 00:30:44 +0900

    예시 형식으로
    gitignore삭제, config.php 예시 형식을 추가

---
+++ back/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('JWT_SECRET_KEY','abc123!@#');//영어,숫자,특수문자로 구성된 아무 값
+define('MAIL_USERNAME','네이버메일@naver.com');
+define('MAIL_PASSWORD','12자리 영어/숫자 비밀번호');// 네이버 애플리케이션 비밀번호
+define('DB_HOST','localhost');
+define('DB_NAME','db_signup');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+
+?>
